d5d3f4b78aa7511a2a804158faadf78aca8bc05b
Nikola Neychev <nikneichev@abv.bg>
2026-04-23 14:59:10 +0300

    RegisterCommand class added

---
+++ src/commands/RegisterCommand.java
@@ -0,0 +1,27 @@
+package commands;
+
+import data.User;
+import java.util.Map;
+
+public class RegisterCommand implements Command{
+
+    private final String username;
+    private final String password;
+    private final Map<String, User> users;
+
+    public RegisterCommand(String username, String password, Map<String, User> users) {
+        this.username = username;
+        this.password = password;
+        this.users = users;
+    }
+
+    @Override
+    public String execute() {
+        User newUser = new User(username, password);
+
+        if (users.putIfAbsent(username, newUser) == null) {
+            return "Successfully registered user " + username;
+        }
+        return "User with username " + username + "already exists.";
+    }
+}
