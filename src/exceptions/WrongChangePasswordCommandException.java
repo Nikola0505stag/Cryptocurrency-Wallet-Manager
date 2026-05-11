0fd216addd005cb471484eb5a23d403aa4f53288
Nikola Neychev <nikneichev@abv.bg>
2026-05-11 17:40:12 +0300

    WrongChangePasswordException added

---
+++ src/exceptions/WrongChangePasswordCommandException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class WrongChangePasswordCommandException extends RuntimeException {
+
+    public WrongChangePasswordCommandException(String message) {
+        super(message);
+    }
+
+    public WrongChangePasswordCommandException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+}
