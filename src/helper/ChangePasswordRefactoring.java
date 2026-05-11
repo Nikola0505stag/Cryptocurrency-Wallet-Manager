04ca340c2b637ecfa6b96dd3ce20a04819b39927
Nikola Neychev <nikneichev@abv.bg>
2026-05-11 17:44:41 +0300

    ChangePasswordRefactoring added

---
+++ src/helper/ChangePasswordRefactoring.java
@@ -0,0 +1,31 @@
+package helper;
+
+import exceptions.NegativeDepositException;
+import exceptions.WrongChangePasswordCommandException;
+import exceptions.WrongDepositCommandException;
+import exceptions.ZeroDepositException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ChangePasswordRefactoring {
+
+    private static final Pattern CHANGE_PASSWORD_PATTERN =
+            Pattern.compile("^change-password\\s--old=(\\S+)\\s--new=(\\S+)$");
+
+    public static PasswordChangeDetails parseChangePassword(String input) {
+        Matcher matcher = CHANGE_PASSWORD_PATTERN.matcher(input);
+
+        if (matcher.matches()) {
+            String oldPassword = matcher.group(1);
+            String newPassword = matcher.group(2);
+
+            return new PasswordChangeDetails(oldPassword, newPassword);
+        }
+
+        throw new WrongChangePasswordCommandException("Invalid change password command! \n" +
+                " Format must be: change-password --old-password<pass> --new-password=<pass>");
+
+    }
+
+}
