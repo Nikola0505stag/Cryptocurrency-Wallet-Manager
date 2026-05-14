bb5dbb6c623cc6bb87652c7846f1b4eb341a1fad
Nikola Neychev <nikneichev@abv.bg>
2026-05-14 18:05:39 +0300

    ListOfferRefactoring added

---
+++ src/helper/ListOfferRefactoring.java
@@ -0,0 +1,24 @@
+package helper;
+
+import exceptions.WrongListOfferCommandException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ListOfferRefactoring {
+    private static final Pattern LIST_OFFER_PATTERN =
+            Pattern.compile("^list-offer=(\\S+)$");
+
+    public static String parseChangePassword(String input) {
+        Matcher matcher = LIST_OFFER_PATTERN.matcher(input);
+
+        if (matcher.matches()) {
+            String cryptoName = matcher.group(1);
+
+            return cryptoName;
+        }
+
+        throw new WrongListOfferCommandException("Invalid list offer command! \n" +
+                " Format must be: \"list-offer=<name>\"");
+
+    }
+}
